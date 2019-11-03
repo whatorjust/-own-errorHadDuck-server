7b5b603c68f7e2ba3ecf935e969f7f1d461b91ad
liftingturn <0oooceanhigh@gmail.com>
2019-11-03 22:38:21 +0900

    :bug: fix keyword overlap, add comment, shorten code
    keyword 중복생성 고침, postscontroller 주석추가, 불필요한코드 삭제

--- controller/postsController.js
+++ controller/postsController.js
@@ -3,57 +3,63 @@ const models = require('../models');
 module.exports = {
   newPost: async function(req, res) {
     try {
+      //해당 유저에 연결시켜주기 위해 해당 유저객체 find
       models.User.findOne({
         where: {
           id: req.body.userid
         }
       }).then(user => {
+        //해당 유저의 post생성
         models.Post.create({
           UserId: user.id,
           postname: req.body.post.postname,
           postcode: req.body.post.postcode,
           solution: req.body.post.solution
-        })
-          .then(result => {
-            return result.dataValues;
-          })
-          .then(post => {
-            if (req.body.refer) {
-              //refers array가 있는 경우
-              Promise.all(
-                req.body.refer.map(refer => {
-                  models.Refer.create({
-                    PostId: post.id,
-                    referurl: refer.referurl,
-                    understand: refer.understand
-                  });
-                })
-              ).then(() => {
-                if (req.body.keyword) {
-                  models.Post.findOne({ where: { id: post.id } }).then(post => {
-                    Promise.all(
-                      req.body.keyword.map(keyword => {
-                        models.Keyword.create({
-                          keyword: keyword
-                        }).then(keyword => {
-                          models.Poskey.create({
-                            KeywordId: keyword.id,
-                            PostId: post.id
+        }).then(post => {
+          if (req.body.refer) {
+            //refers array가 있는 경우 반복문 비동기 처리를 통해, 다수의 refer객체 생성
+            Promise.all(
+              req.body.refer.map(refer => {
+                models.Refer.create({
+                  PostId: post.id,
+                  referurl: refer.referurl,
+                  understand: refer.understand
+                });
+              })
+            ).then(() => {
+              //keyword 항목 존재 경우 반복문 비동기 처리를 통해, 다수의 Keyword객체 생성
+              if (req.body.keyword) {
+                Promise.all(
+                  req.body.keyword.map(keyword => {
+                    models.Keyword.findOne({ where: { keyword: keyword } }) //이미 존재하는 keyword인지 확인
+                      .then(found => {
+                        if (found) {
+                          return found;
+                        } else {
+                          //새로운 keyword일 시에만 keyword생성
+                          models.Keyword.create({
+                            keyword: keyword
                           });
+                        }
+                      }) //해당 keyword의 id를 조인테이블을 통해 post와 연결해줌
+                      .then(keyword => {
+                        models.Poskey.create({
+                          KeywordId: keyword.id,
+                          PostId: post.id
                         });
-                      })
-                    ).then(() => {
-                      res.status(200).send({ postid: post.id });
-                    });
-                  });
-                } else {
+                      });
+                  })
+                ).then(() => {
                   res.status(200).send({ postid: post.id });
-                }
-              });
-            } else {
-              res.status(200).send({ postid: post.id });
-            }
-          });
+                });
+              } else {
+                res.status(200).send({ postid: post.id });
+              }
+            });
+          } else {
+            res.status(200).send({ postid: post.id });
+          }
+        });
       });
     } catch (err) {
       res.sendStatus(500);
@@ -62,21 +68,19 @@ module.exports = {
   getOne: function(req, res) {
     try {
       console.log('id', req.params.id);
-      // res.send(req.params.id);
-      models.Post.findAll({
+      models.Post.findOne({
+        //입력받은 postid를 통해 post선택
         where: {
           id: req.params.id
-        },
+        }, //참조관계인 model:Poskey도 포함
         include: [
           {
-            model: models.Poskey,
+            model: models.Poskey, //연관된 models.Poskey에 연관된 models.Keyword도 포함
             include: { model: models.Keyword }
           },
-          { model: models.Refer }
+          { model: models.Refer } //참조관계인 model:Refer 포함
         ]
       }).then(result => {
-        console.log('----', result.length);
-        console.log('----', result.Poskeys);
         res.send(result);
       });
     } catch (err) {
